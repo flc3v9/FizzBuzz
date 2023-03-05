b253118d505e242104fe369599cfd0efdc67a49a
flc3v9 <97908586+flc3v9@users.noreply.github.com>
2023-03-05 10:42:54 -0600

    changed range and first if condition

--- efficiency.py
+++ efficiency.py
@@ -1,16 +1,15 @@
 # this is where I will increase efficiency for program
 def main():
 
-    x = 1
-    for x in range(1,100):
-        if(x % 3 == 0 and x % 5 == 0):
+    for x in range(1,101):
+        if(x % 15 == 0):
             print("fizzbuzz")
-            continue
-        elif(x % 5 == 0):
-            print("buzz")
             continue
         elif(x % 3 == 0):
             print("fizz")
+            continue
+        elif(x % 5 == 0):
+            print("buzz")
             continue
         print(x)
 
